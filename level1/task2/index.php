728590381568cfbcc6d260a3d370a6d86ed7be34
TimKorAnd <timkorand@gmail.com>
2020-03-12 21:15:59 +0200

    tester added, task2 structure changed

--- level1/task2/index.php
+++ level1/task2/index.php
@@ -7,6 +7,7 @@ function readHttpLikeInput() {
     $store = "";
     $toread = 0;
     while( $line = fgets( $f ) ) {
+        echo $line;
         $store .= preg_replace("/\r/", "", $line);
         if (preg_match('/Content-Length: (\d+)/',$line,$m))
             $toread=$m[1]*1;
@@ -15,19 +16,21 @@ function readHttpLikeInput() {
     }
     if ($toread > 0)
         $store .= fread($f, $toread);
+    echo $store;
     return $store;
 }
 
-$contents = readHttpLikeInput();
+//$contents = readHttpLikeInput();
 
-function parseTcpStringAsHttpRequest($string) {
+/*function parseTcpStringAsHttpRequest($string) {
     return array(
         "method" => ...,
         "uri" => ...,
         "headers" => ...,
         "body" => ...,
     );
-}
+}*/
 
-$http = parseTcpStringAsHttpRequest($contents);
-echo(json_encode($http, JSON_PRETTY_PRINT));
+/*$http = parseTcpStringAsHttpRequest($contents);
+echo(json_encode($http, JSON_PRETTY_PRINT));*/
+echo("hello...");
