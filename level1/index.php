bf96b0a7f0d12c2fabc1b421aea92e3cd1294b2b
TimKorAnd <timkorand@gmail.com>
2020-03-12 19:07:03 +0200

    --amend

---
+++ level1/index.php
@@ -0,0 +1,33 @@
+<?php
+
+// не обращайте на эту функцию внимания
+// она нужна для того чтобы правильно считать входные данные
+function readHttpLikeInput() {
+    $f = fopen( 'php://stdin', 'r' );
+    $store = "";
+    $toread = 0;
+    while( $line = fgets( $f ) ) {
+        $store .= preg_replace("/\r/", "", $line);
+        if (preg_match('/Content-Length: (\d+)/',$line,$m))
+            $toread=$m[1]*1;
+        if ($line == "\r\n")
+            break;
+    }
+    if ($toread > 0)
+        $store .= fread($f, $toread);
+    return $store;
+}
+
+$contents = readHttpLikeInput();
+
+function parseTcpStringAsHttpRequest($string) {
+    return array(
+        "method" => ...,
+        "uri" => ...,
+        "headers" => ...,
+        "body" => ...,
+    );
+}
+
+$http = parseTcpStringAsHttpRequest($contents);
+echo(json_encode($http, JSON_PRETTY_PRINT));
